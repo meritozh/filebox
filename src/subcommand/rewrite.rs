dece6d85c655027dfb84dc9d8d1e4bb6ef318a7d
meritozh <ah841814092@gmail.com>
2023-02-12 23:53:11 +0800

    feat: add pest parser code template

--- src/subcommand/rewrite.rs
+++ src/subcommand/rewrite.rs
@@ -3,37 +3,59 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-use std::{path::Path, fs::File, io::{BufReader, BufRead}};
+use std::{path::Path, fs::File, io::Read};
 
 use crate::utils::get_canonicalize_path;
 
+use pest::Parser;
 use pest_derive::Parser;
 
 struct Rewrite {
 
 }
 
+#[derive(Parser)]
+#[grammar = "workflow.pest"]
 struct Workflow<'a> {
     nodes: Vec<Node<'a>>
 }
 
 impl<'a> Workflow<'a> {
-    // pub fn new<P: AsRef<Path>>(path: P) -> Self {
-    //     let pathbuf = get_canonicalize_path(path.as_ref());
-    //     let file = File::open(pathbuf).expect("file not exist or denied open");
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        todo!()
+        // let pathbuf = get_canonicalize_path(path.as_ref());
+        // let file = File::open(pathbuf).expect("file not exist or denied open");
 
-    //     Self { nodes: vec![] }
-    // }
+        // Self { nodes: vec![] }
+    }
 
     fn parse_from(file: File) {
-        let stream = BufReader::new(file);
-
+        let pairs = Workflow::parse(Rule::flow, "test here");
+        match pairs {
+            Ok(paris) => {
+                paris.for_each(|p| {
+                    match p.as_rule() {
+                        Rule::node => todo!(),
+                        Rule::normalize => todo!(),
+                        Rule::form => todo!(),
+                        Rule::recode => todo!(),
+                        Rule::target => todo!(),
+                        Rule::encoding => todo!(),
+                        Rule::rename => todo!(),
+                        Rule::command => todo!(),
+                        Rule::pattern => todo!(),
+                        Rule::inner => todo!(),
+                        Rule::flow => todo!(),
+                        _ => unreachable!()
+                    }
+                });
+            }
+            Err(err) => {
+                eprintln!("Failed parsing input: {:}", err);
+            }
+        }
     }
 }
-
-#[derive(Parser)]
-#[grammar = "workflow.pest"]
-struct Parser;
 
 enum Node<'a> {
     Normalize(NormalizeNode<'a>),
