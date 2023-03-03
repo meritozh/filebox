6cb10d5e10327fb4e8b7755a624e4ee626037d19
meritozh <ah841814092@gmail.com>
2023-03-03 08:47:58 +0800

    feat: try to remove nightly feature

--- src/subcommand/workflow.rs
+++ src/subcommand/workflow.rs
@@ -3,14 +3,17 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+use std::io::{self, BufWriter};
+use std::path::PathBuf;
 use std::{fs::File, io::Read, path::Path};
 
-use crate::utils::get_canonicalize_path;
 use crate::subcommand::executor::Executor;
+use crate::utils::{get_canonicalize_path, is_hidden};
 
 use encoding_rs::Encoding;
 use pest::Parser;
 use pest_derive::Parser;
+use walkdir::WalkDir;
 
 type Task = impl Fn(&[u8]) -> String;
 
@@ -35,7 +38,7 @@ impl<'a> Workflow<'a> {
         Self {
             source_content: buf,
             nodes: None,
-            executor: Executor::new()
+            executor: Executor::new(),
         }
     }
 
@@ -114,40 +117,106 @@ impl<'a> Workflow<'a> {
 
     pub fn run(&self) {
         if let Some(ref nodes) = self.nodes {
-            let path = nodes
+            let Node::Path(path) = nodes
                 .iter()
                 .find(|n| matches!(n, Node::Path(_)))
                 .expect("must provide PATH node");
 
-            nodes.iter().for_each(|node| match node {
-                Node::Path(_) => {
-                    // Ignore Path node
-                }
-                Node::Normalize(node) => match (&node.from, &node.to) {
-                    (Form::Nfc, Form::Nfd) => {
-                        self.executor.add_task(|input| input);
+            let Node::Target(target) = nodes
+                .iter()
+                .find(|n| matches!(n, Node::Target(_)))
+                .expect("must provide TARGET node");
+
+            if matches!(target, Target::Content) {
+                panic!("current not implement Content TARGET");
+            }
+
+            let pathbuf = get_canonicalize_path(path.as_ref());
+            let walkdir = WalkDir::new(pathbuf);
+            let output = File::create("workflow-results.olog").unwrap();
+
+            let mut stream = BufWriter::new(output);
+
+            walkdir
+                .into_iter()
+                .filter_entry(|e| !is_hidden(e))
+                .for_each(|file| {
+                    if let Ok(pathbuf) = file.map(|f| f.into_path()) {
+                        let x = nodes
+                            .iter()
+                            .fold::<Option<PathBuf>, _>(None, |input, node| {
+                                let path = if input.is_none() {
+                                    pathbuf
+                                } else {
+                                    input.unwrap()
+                                }
+                                .as_path();
+
+                                let x = match node {
+                                    Node::Path(_) | Node::Target(_) => input.into(),
+                                    Node::Normalize(node) => match (&node.from, &node.to) {
+                                        (Form::Nfc, Form::Nfd) => convert_to_nfd(path),
+                                        (Form::Nfd, Form::Nfc) => convert_to_nfc(path),
+                                        _ => unreachable!(),
+                                    },
+                                    Node::Recode(node) => match node.encoding {
+                                        Some((from, to)) => {
+                                            let from = Encoding::for_label(from.as_bytes())
+                                                .expect("from encoding isn't correct");
+                                            let to = Encoding::for_label(to.as_bytes())
+                                                .expect("to encoding isn't correct");
+
+                                            change_encoding(from, to, path)
+                                        }
+                                        None => {}
+                                    },
+                                    Node::Rename(node) => match node.command {
+                                        Command::Remove => {
+                                            node.pattern.iter().for_each(|pat| todo!());
+                                        }
+                                    },
+                                    _ => unreachable!(),
+                                };
+
+                                None
+                            });
                     }
-                    (Form::Nfd, Form::Nfc) => {}
-                    _ => unreachable!(),
-                },
-                Node::Recode(node) => match node.encoding {
-                    Some((from, to)) => {
-                        let from = Encoding::for_label(from.as_bytes())
-                            .expect("from encoding isn't correct");
-                        let to =
-                            Encoding::for_label(to.as_bytes()).expect("to encoding isn't correct");
-                    }
-                    None => todo!(),
-                },
-                Node::Rename(node) => match node.command {
-                    Command::Remove => {
-                        node.pattern.iter().for_each(|pat| todo!());
-                    }
-                },
-                _ => unreachable!(),
-            });
+                });
+
+            stream.flush()?;
         }
     }
+}
+
+fn convert_to_nfc(path: &Path) -> PathBuf {
+    if let Some(filename) = path.file_name().and_then(|n| n.to_str()) {
+        if is_nfd(filename) || is_nfkd(filename) {
+            let normalized_filename = filename.nfc().collect::<String>();
+            return path.with_file_name(normalized_filename);
+        }
+    }
+
+    path.to_path_buf()
+}
+
+fn convert_to_nfd(path: &Path) -> PathBuf {
+    if let Some(filename) = path.file_name().and_then(|n| n.to_str()) {
+        if is_nfc(filename) || is_nfkc(filename) {
+            let normalized_filename = filename.nfc().collect::<String>();
+            return path.with_file_name(normalized_filename);
+        }
+    }
+
+    path.to_path_buf()
+}
+
+fn change_encoding(from: &Encoding, to: &Encoding, path: &Path) -> PathBuf {
+    if let Some(filename) = path.file_name() {
+        let (from_encoded, _, _) = from.encode(filename.to_str().unwrap());
+        let (to_decoded, _, _) = to.decode(from_encoded.as_ref());
+        return path.with_file_name(to_decoded);
+    }
+    path.into()
 }
 
 enum Node<'a> {
